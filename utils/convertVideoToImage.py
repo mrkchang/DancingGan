7aec19a9fd06946e6d4752f41901f065019cf3a3
markcha <Mark.chang@intusurg.com>
2019-05-11 15:23:08 -0700

    test

--- utils/convertVideoToImage.py
+++ utils/convertVideoToImage.py
@@ -1,16 +1,18 @@
 import cv2
 import os
 print(cv2.__version__)
-vidcap = cv2.VideoCapture('../data/original.mp4')
+vidcap = cv2.VideoCapture('../data/poses.avi')
+savefolder = "train_A"
+
 success, image = vidcap.read()
 count = 0
 success = True
 
-if not os.path.exists("../data/original_images"):
-    os.makedirs("../data/original_images")
+if not os.path.exists("../data/" + savefolder):
+    os.makedirs("../data/" + savefolder)
 
 while success:
-    cv2.imwrite("../data/original_images/frame%d.jpg" % count, image)     # save frame as JPEG file
+    cv2.imwrite("../data/" + savefolder +"/frame%d.jpg" % count, image)     # save frame as JPEG file
     success,image = vidcap.read()
     print('Read a new frame', count, ' : ', success)
     count += 1
