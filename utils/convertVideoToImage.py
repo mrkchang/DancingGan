f015694e92f9ee4487e8c5628aca40f5667ca596
michaelArruza <marruza@stanford.edu>
2019-05-11 11:39:43 -0700

    altered video to image converter

--- utils/convertVideoToImage.py
+++ utils/convertVideoToImage.py
@@ -1,16 +1,16 @@
 import cv2
 import os
 print(cv2.__version__)
-vidcap = cv2.VideoCapture('../data/posesonly.avi')
+vidcap = cv2.VideoCapture('../data/original.mp4')
 success, image = vidcap.read()
 count = 0
 success = True
 
-if not os.path.exists("../data/posesonly_images"):
-    os.makedirs("../data/posesonly_images")
+if not os.path.exists("../data/original_images"):
+    os.makedirs("../data/original_images")
 
 while success:
-    cv2.imwrite("../data/posesonly_images/frame%d.jpg" % count, image)     # save frame as JPEG file
+    cv2.imwrite("../data/original_images/frame%d.jpg" % count, image)     # save frame as JPEG file
     success,image = vidcap.read()
-    print('Read a new frame: ', success)
+    print('Read a new frame', count, ' : ', success)
     count += 1
