030a7818aaaa8a1d23147e1bb5221c6f3ba82588
michaelArruza <marruza@stanford.edu>
2019-06-01 16:10:39 -0700

    new ssim

--- utils/ssim.py
+++ utils/ssim.py
@@ -1,22 +1,25 @@
 from skimage.measure import compare_ssim
 from skimage.io import imread
 import matplotlib.pyplot as plt
-
+import pickle
 import os
 
-ground_truth_dir =  "" #Put directories here; must have files with same name.
-generated_image_dir = ""
+ground_truth_dir =  "thirdparty/pix2pixHD/datasets/poses/train_B" #Put directories here; must have files with same name.
+generated_image_dir = "thirdparty/pix2pixHD/results/backgroundWithRandomKeep/test_latest/images"
 
 ssim_scores = []
 
-for filename in os.listdir(ground_truth_dir):
-    true_image = imread(os.path.join(ground_truth_dir, filename))
-    gen_image = imread(os.path.join(generated_image_dir, filename))
+for i in range(1,33073):
+    real_filename = "frame" + str(i) + ".jpg"
+    synth_filename = "frame" + str(i) + "_synthesized_image.jpg"
+    true_image = imread(os.path.join(ground_truth_dir, real_filename))
+    gen_image = imread(os.path.join(generated_image_dir, synth_filename))
     ssim_scores.append(compare_ssim(true_image, gen_image))
 
 print("Max SSIM: ", max(ssim_scores))
 print("Min SSIM: ", min(ssim_scores))
 print("Mean SSIM: ", sum(ssim_scores)/len(ssim_scores))
 
-plt.hist(ssim_scores, 100)
-plt.show()
+with open('list.pkl', 'wb') as f:
+    pickle.dump(ssim_scores, f)
+
